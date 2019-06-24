54d4e9568e57f2ed3147be9c617cf3e85bcdb536
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-06-24 11:34:52 +0200

    fix benchmark script

--- scripts/warnke_skript/benchmark.sh
+++ scripts/warnke_skript/benchmark.sh
@@ -1,28 +1,30 @@
 umount /mnt2
 mount /mnt2
 
-./build/tools/julea-config --user \
+
+./scripts/warnke_skript/kill.sh
+./scripts/warnke_skript/format.sh
+./waf.sh configure --out build-gcc-benchmark --prefix=prefix-gcc-benchmark --libdir=prefix-gcc-benchmark --bindir=prefix-gcc-benchmark --destdir=prefix-gcc-benchmark
+# --debug --sanitize --hdf5=./dependencies/opt/spack/linux-ubuntu18.10-x86_64/gcc-8.3.0/hdf5-develop-qckzbb5gxnzeixlnhtgkq5mxvavegx4n
+./waf.sh clean
+./waf.sh build
+./waf.sh install
+rm -rf /mnt2/julea/*
+export LD_LIBRARY_PATH=$(pwd)/prefix-gcc-benchmark/lib/:$LD_LIBRARY_PATH
+export JULEA_CONFIG=~/.config/julea/julea-benchmark
+./build-gcc-benchmark/tools/julea-config --user \
   --object-servers="$(hostname)" --kv-servers="$(hostname)" \
   --smd-servers="$(hostname)" \
   --object-backend=posix --object-component=server --object-path=/mnt2/julea/object \
   --kv-backend=sqlite --kv-component=server --kv-path=/mnt2/julea/kv \
   --smd-backend=sqlite --smd-component=server --smd-path=/mnt2/julea/smd
-
-pkill julea-server
-
-./scripts/warnke_skript/format.sh
-./waf.sh configure
-# --debug --sanitize --hdf5=./dependencies/opt/spack/linux-ubuntu18.10-x86_64/gcc-8.3.0/hdf5-develop-qckzbb5gxnzeixlnhtgkq5mxvavegx4n
-./waf.sh clean
-./waf.sh build
-./waf.sh install -f
-rm -rf /mnt2/julea/*
+mv ~/.config/julea/julea ~/.config/julea/julea-benchmark
 julea-server &
 sleep 5
 githash=$(git log --pretty=format:'%H' -n 1)
 rm -rf benchmark_values/warnke-${githash}
 mkdir benchmark_values/warnke-${githash}
 cd benchmark_values/warnke-${githash}
-../../build/benchmark/julea-benchmark >> benchmark_values
-pkill julea-server
+../../build-gcc-benchmark/benchmark/julea-benchmark >> benchmark_values
 cd ../..
+./scripts/warnke_skript/kill.sh
