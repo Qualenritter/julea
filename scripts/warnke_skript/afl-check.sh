966a16717803ff12a43924794a3b2d2e609e59cc
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-06-25 15:40:10 +0200

    avoid inferference with checking for valid testcases while running afl in background

--- scripts/warnke_skript/afl-check.sh
+++ scripts/warnke_skript/afl-check.sh
@@ -2,12 +2,23 @@ files=$(ls afl/start-files/* | grep -v README )
 files="${files} $(ls afl/out/*/crashes/* | grep -v README )"
 rm -rf b
 mkdir b
+
+i=300
+(export LD_LIBRARY_PATH=prefix-clang/lib/:$LD_LIBRARY_PATH; ./build-clang/tools/julea-config --user \
+  --object-servers="$(hostname)" --kv-servers="$(hostname)" \
+  --smd-servers="$(hostname)" \
+  --object-backend=posix --object-component=client --object-path="/mnt2/julea/object${i}" \
+  --kv-backend=sqlite --kv-component=client --kv-path="/mnt2/julea/kv${i}" \
+  --smd-backend=sqlite --smd-component=client --smd-path=":memory:")
+mv ~/.config/julea/julea ~/.config/julea/julea${i}
+
+
 for g in gcc-gcov-debug-asan clang gcc-gcov
 do
 mkdir b/${g}
 for f in ${files}
 do
-	(export export G_DEBUG=resident-modules; export G_MESSAGES_DEBUG=all; export LD_LIBRARY_PATH=prefix-${g}/lib/:$LD_LIBRARY_PATH; export JULEA_CONFIG=~/.config/julea/julea1; export AFL_USE_ASAN=1; export ASAN_OPTIONS=fast_unwind_on_malloc=0,abort_on_error=1,symbolize=0; cat $f | ./build-${g}/test-afl/julea-test-afl > x 2>&1)
+	(export export G_DEBUG=resident-modules; export G_MESSAGES_DEBUG=all; export LD_LIBRARY_PATH=prefix-${g}/lib/:$LD_LIBRARY_PATH; export JULEA_CONFIG=~/.config/julea/julea${i}; export AFL_USE_ASAN=1; export ASAN_OPTIONS=fast_unwind_on_malloc=0,abort_on_error=1,symbolize=0; cat $f | ./build-${g}/test-afl/julea-test-afl > x 2>&1)
 	r=$?
 	if [ $r -eq 0 ]; then
 		echo "invalid $f $g"
---
+++ x
@@ -0,0 +1,14 @@
+
+** (process:11414): CRITICAL **: 15:38:54.109: ../backend/smd/sqlite.c:585:backend_init: :memory:
+
+** (process:11414): CRITICAL **: 15:38:54.109: ../backend/smd/sqlite.c:597:backend_init: useing path=:memory:
+
+** (process:11414): CRITICAL **: 15:38:54.121: ../backend/smd/sqlite.c:619:backend_init: :memory:
+
+** (process:11414): CRITICAL **: 15:38:54.121: ../lib/smd/jsmd.c:62:j_smd_reset: reset in lib0
+
+** (process:11414): CRITICAL **: 15:38:54.121: ../lib/smd/jsmd.c:75:j_smd_reset: smd local reset0
+
+** (process:11414): CRITICAL **: 15:38:54.121: ../lib/smd/jsmd.c:90:j_smd_reset: object local reset0
+
+** (process:11414): CRITICAL **: 15:38:54.121: ../backend/object/posix.c:384:backend_reset: reset posix0
