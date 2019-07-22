4194e3b68b623ab955351c7376ea18eeeb63e27d
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-07-22 07:33:01 +0200

    fixes

--- test-afl/test-db-selector.h
+++ test-afl/test-db-selector.h
@@ -188,6 +188,7 @@ event_selector_add_selector(void)
 	{
 	case 0:
 		ret_expected = ret_expected && (stored_selectors[random_values.namespace][random_values.name][random_values.selector_selector] != NULL);
+		ret_expected = ret_expected && (stored_selectors[random_values.namespace][random_values.name][random_values.selector_selector]->bson_count);
 		ret_expected = ret_expected && random_values.selector_selector != random_values.selector;
 		ret = j_db_selector_add_selector(the_stored_selector, stored_selectors[random_values.namespace][random_values.name][random_values.selector_selector], &error);
 		J_AFL_DEBUG_ERROR(ret, ret_expected, error);
