518d076cb99640d17fe2382c9eb18e6f0c748332
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-08-15 22:18:27 +0200

    better range handling in hdf5

--- lib/hdf5/jhdf5-db-dataset.c
+++ lib/hdf5/jhdf5-db-dataset.c
@@ -400,26 +400,6 @@ _error:
 	H5VL_julea_db_object_unref(object);
 	return NULL;
 }
-static hssize_t
-H5VL_julea_db_arrayindex_to_linearindex(gint ndims, hsize_t* dims, hsize_t* arr_index)
-{
-	J_TRACE_FUNCTION(NULL);
-
-	hsize_t skipsize;
-	hsize_t linear_index;
-	gint i;
-
-	g_return_val_if_fail(ndims < 1, -1);
-
-	linear_index = arr_index[ndims - 1];
-	skipsize = dims[ndims - 1];
-	for (i = 2; i <= ndims; i++)
-	{
-		linear_index += arr_index[ndims - i] * skipsize;
-		skipsize *= dims[ndims - i];
-	}
-	return linear_index;
-}
 static herr_t
 H5VL_julea_db_dataset_read(void* obj, hid_t mem_type_id, hid_t mem_space_id, hid_t file_space_id,
 	hid_t xfer_plist_id, void* buf, void** req)
@@ -453,55 +433,6 @@ H5VL_julea_db_dataset_read(void* obj, hid_t mem_type_id, hid_t mem_space_id, hid
 		{
 		case H5S_SEL_POINTS:
 		{
-			/*			hssize_t npoints = 0;
-			hsize_t file_offset = 0;
-			hsize_t point_current_arr[ndims];
-			hssize_t point_range_first = 0;
-			hssize_t point_range_last = 0;
-			hssize_t point_current = 0;
-			hssize_t point_current_index = 0;
-
-			if ((npoints = H5Sget_select_elem_npoints(mem_space_id)) <= 0)
-			{
-				goto _error;
-			}
-		_start:
-			if (point_current_index < npoints)
-			{
-				if (H5Sget_select_elem_pointlist(mem_space_id, point_current_index, 1, point_current_arr) < 0)
-				{
-					goto _error;
-				}
-				if ((point_current = H5VL_julea_db_arrayindex_to_linearindex(ndims, dims, point_current_arr)) < 0)
-				{
-					goto _error;
-				}
-				point_range_first = point_current;
-				point_range_last = point_current;
-				while (point_current_index < npoints)
-				{
-					if (H5Sget_select_elem_pointlist(mem_space_id, point_current_index, 1, point_current_arr) < 0)
-					{
-						goto _error;
-					}
-					if ((point_current = H5VL_julea_db_arrayindex_to_linearindex(ndims, dims, point_current_arr)) < 0)
-					{
-						goto _error;
-					}
-					if (point_current == point_range_last + 1)
-					{
-						point_range_last++;
-					}
-					else
-					{
-						j_distributed_object_read(object->dataset.object, ((char*)buf) + data_size * file_offset, data_size * (point_range_last - point_range_first + 1), data_size * file_offset, &bytes_read, batch);
-						file_offset += point_range_last - point_range_first + 1;
-						g_assert(bytes_read == data_size * (point_range_last - point_range_first + 1));
-						goto _start;
-					}
-					point_current_index++;
-				}
-			}*/
 			g_critical("%s NOT implemented !!", G_STRLOC);
 			abort();
 		}
@@ -551,70 +482,282 @@ H5VL_julea_db_dataset_read(void* obj, hid_t mem_type_id, hid_t mem_space_id, hid
 _error:
 	return 1;
 }
+
+struct JHDF5IndexRange
+{
+	guint64 start;
+	guint64 stop;
+};
+typedef struct JHDF5IndexRange JHDF5IndexRange;
+
+static GArray*
+H5VL_julea_db_space_hdf5_to_range(hid_t mem_space_id, hid_t stored_space_id)
+{
+	g_autofree hsize_t* stored_dims = NULL;
+	JHDF5IndexRange range;
+	JHDF5IndexRange range_tmp;
+	GArray* range_arr = NULL;
+	gboolean range_valid = FALSE;
+	gint stored_ndims;
+	guint i;
+	stored_ndims = H5Sget_simple_extent_ndims(stored_space_id);
+	stored_dims = g_new(hsize_t, stored_ndims);
+	H5Sget_simple_extent_dims(stored_space_id, stored_dims, NULL);
+	range_arr = g_array_new(FALSE, FALSE, sizeof(JHDF5IndexRange));
+
+	switch (H5Sget_select_type(mem_space_id))
+	{
+	case H5S_SEL_POINTS:
+	{
+		hssize_t npoints = 0;
+		hssize_t point_current = 0;
+		hssize_t point_current_index = 0;
+		g_autofree hsize_t* point_current_arr;
+		guint skipsize;
+
+		point_current_arr = g_new(hsize_t, stored_ndims);
+		if ((npoints = H5Sget_select_elem_npoints(mem_space_id)) <= 0)
+		{
+			goto _error;
+		}
+	_start:
+		while (point_current_index < npoints)
+		{
+			if (H5Sget_select_elem_pointlist(mem_space_id, point_current_index, 1, point_current_arr) < 0)
+			{
+				goto _error;
+			}
+			point_current = point_current_arr[stored_ndims - 1];
+			skipsize = stored_dims[stored_ndims - 1];
+			for (i = 2; i <= (guint)stored_ndims; i++)
+			{
+				point_current += point_current_arr[stored_ndims - i] * skipsize;
+				skipsize *= stored_dims[stored_ndims - i];
+			}
+			if (range_valid)
+			{
+				if ((guint64)point_current == range.stop + 1)
+				{
+					range.stop++;
+				}
+				else
+				{
+					g_array_append_val(range_arr, range);
+					range.start = point_current;
+					range.stop = point_current;
+				}
+			}
+			else
+			{
+				range.start = point_current;
+				range.stop = point_current;
+				range_valid = TRUE;
+				goto _start;
+			}
+			point_current_index++;
+		}
+		if (range_valid)
+		{
+			g_array_append_val(range_arr, range);
+		}
+	}
+	break;
+	case H5S_SEL_HYPERSLABS:
+	{
+		g_autofree hsize_t* hyperspab_counters = NULL;
+		g_autofree hsize_t* hyperspab_coordinates = NULL;
+		g_autofree hsize_t* hyperspab_skip_size = NULL;
+		hssize_t hyperslab_count;
+		hssize_t hyperslab_index;
+		gint current_counter;
+		gboolean found;
+		gboolean first;
+
+		if ((hyperslab_count = H5Sget_select_hyper_nblocks(mem_space_id)) < 0)
+			goto _error;
+		hyperspab_coordinates = g_new(hsize_t, stored_ndims * 2);
+		hyperspab_counters = g_new(hsize_t, stored_ndims);
+		hyperspab_skip_size = g_new(hsize_t, stored_ndims);
+		hyperspab_skip_size[stored_ndims - 1] = 1;
+		for (i = stored_ndims - 1; i > 0; i--)
+		{
+			hyperspab_skip_size[i - 1] = hyperspab_skip_size[i] * stored_dims[i];
+		}
+		//iterate over all hyperslabs
+		for (hyperslab_index = 0; hyperslab_index < hyperslab_count; hyperslab_index++)
+		{
+			if (H5Sget_select_hyper_blocklist(mem_space_id, hyperslab_index, 1, hyperspab_coordinates) < 0)
+				goto _error;
+
+			for (i = 0; i < (guint)stored_ndims; i++)
+			{
+				hyperspab_counters[i] = hyperspab_coordinates[i];
+			}
+			current_counter = stored_ndims - 1;
+			first = TRUE;
+			//within a hyperslab - iterate over the dimensions
+
+			do
+			{
+				//update current counter
+				found = FALSE;
+				while (current_counter >= 0)
+				{
+					if ((hyperspab_counters[current_counter] < hyperspab_coordinates[stored_ndims + current_counter]) || first)
+					{
+						found = TRUE;
+						first = FALSE;
+						break;
+					}
+					for (i = 0; i < (guint)stored_ndims; i++)
+					{
+						//reset counters
+						hyperspab_counters[current_counter] = hyperspab_coordinates[i];
+					}
+					current_counter--;
+				}
+				if (!found)
+					break;
+				//calculate output range
+				range_tmp.start = 0;
+				for (i = 0; i < (guint)stored_ndims; i++)
+				{
+					range_tmp.start += hyperspab_counters[i] * hyperspab_skip_size[i];
+				}
+				range_tmp.stop = range_tmp.start + hyperspab_coordinates[stored_ndims * 2 - 1] - hyperspab_coordinates[stored_ndims - 1];
+				if (!range_valid)
+				{
+					range.start = range_tmp.start;
+					range.stop = range_tmp.stop;
+					range_valid = TRUE;
+				}
+				else
+				{
+					if (range.stop + 1 == range_tmp.start)
+					{ //merge consecutive ranges
+						range.stop = range_tmp.stop;
+					}
+					else
+					{ //append range if the next has a gap
+						g_array_append_val(range_arr, range);
+						range.start = range_tmp.start;
+						range.stop = range_tmp.stop;
+					}
+				}
+				hyperspab_counters[current_counter]++;
+			} while (1);
+		}
+		if (range_valid)
+		{
+			g_array_append_val(range_arr, range);
+		}
+	}
+	break;
+	case H5S_SEL_ALL:
+	{
+		range.start = 0;
+		range.stop = 1;
+		for (i = 0; i < (guint)stored_ndims; i++)
+		{
+			range.stop *= stored_dims[i];
+		}
+		g_array_append_val(range_arr, range);
+	}
+	break;
+	case H5S_SEL_N:
+	case H5S_SEL_ERROR:
+	case H5S_SEL_NONE:
+	default:
+		g_critical("%s NOT implemented !!", G_STRLOC);
+		abort();
+	}
+	//TODO for speedup: sort the array of ranges and merge if possible
+	return range_arr;
+_error:
+	g_array_free(range_arr, TRUE);
+	return NULL;
+}
+
 static herr_t
 H5VL_julea_db_dataset_write(void* obj, hid_t mem_type_id, hid_t mem_space_id, hid_t file_space_id,
 	hid_t xfer_plist_id, const void* buf, void** req)
 {
 	J_TRACE_FUNCTION(NULL);
 
+	g_autofree hsize_t* stored_dims = NULL;
+	g_autofree void* local_buf_org;
 	g_autoptr(JBatch) batch = NULL;
+	g_autoptr(GArray) mem_space_arr = NULL;
+	g_autoptr(GArray) file_space_arr = NULL;
+	const void* local_buf;
 	guint64 bytes_written;
+	gint stored_ndims;
 	gsize data_size;
 	gsize data_count;
 	JHDF5Object_t* object = obj;
+	guint mem_space_idx;
+	guint file_space_idx;
+	JHDF5IndexRange* mem_space_range = NULL;
+	JHDF5IndexRange* file_space_range = NULL;
+	guint64 current_count1;
+	guint64 current_count2;
+	guint i;
 
 	g_return_val_if_fail(buf != NULL, 1);
 	g_return_val_if_fail(object->type == J_HDF5_OBJECT_TYPE_DATASET, 1);
 	H5VL_julea_db_datatype_print(mem_type_id);
 	H5VL_julea_db_datatype_print(object->dataset.datatype->datatype.hdf5_id);
 
+	data_size = H5Tget_size(object->dataset.datatype->datatype.hdf5_id);
+	stored_ndims = H5Sget_simple_extent_ndims(object->dataset.space->space.hdf5_id);
+	stored_dims = g_new(hsize_t, stored_ndims);
+	H5Sget_simple_extent_dims(object->dataset.space->space.hdf5_id, stored_dims, NULL);
+
 	if (!(batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT)))
 	{
 		goto _error;
 	}
-	bytes_written = 0;
-	if (file_space_id == H5S_ALL)
+	data_count = 1;
+	for (i = 0; i < (guint)stored_ndims; i++)
 	{
-		if (mem_space_id == H5S_ALL)
-		{
-			g_autofree hsize_t* dims;
-			gint ndims;
-			const void* local_buf;
-			void* local_buf_org;
-			data_size = H5Tget_size(object->dataset.datatype->datatype.hdf5_id);
-			ndims = H5Sget_simple_extent_ndims(object->dataset.space->space.hdf5_id);
-			dims = g_new(hsize_t, ndims);
-			H5Sget_simple_extent_dims(object->dataset.space->space.hdf5_id, dims, NULL);
-			data_count = 1;
-			for (gint i = 0; i < ndims; i++)
-			{
-				data_count *= dims[i];
-			}
-			data_size *= data_count;
-			local_buf_org = g_new(char, data_size);
-			local_buf = H5VL_julea_db_datatype_convert_type(mem_type_id, object->dataset.datatype->datatype.hdf5_id, buf, local_buf_org, data_count);
-			j_distributed_object_write(object->dataset.object, local_buf, data_size, 0, &bytes_written, batch);
-			g_free(local_buf_org);
-		}
-		else
-		{
-			g_critical("%s NOT implemented !!", G_STRLOC);
-			abort();
-		}
+		data_count *= stored_dims[i];
 	}
-	else
+	local_buf_org = g_new(char, data_size* data_count);
+
+	local_buf = H5VL_julea_db_datatype_convert_type(mem_type_id, object->dataset.datatype->datatype.hdf5_id, buf, local_buf_org, data_count);
+	if (!(mem_space_arr = H5VL_julea_db_space_hdf5_to_range(mem_space_id, object->dataset.space->space.hdf5_id)))
+		goto _error;
+	if (!(file_space_arr = H5VL_julea_db_space_hdf5_to_range(file_space_id, object->dataset.space->space.hdf5_id)))
+		goto _error;
+	mem_space_idx = 0;
+	file_space_idx = 0;
+	while ((mem_space_idx < mem_space_arr->len) && (file_space_idx < file_space_arr->len))
 	{
-		if (mem_space_id == H5S_ALL)
+		if (mem_space_range == NULL)
 		{
-			g_critical("%s NOT implemented !!", G_STRLOC);
-			abort();
+			mem_space_range = &g_array_index(mem_space_arr, JHDF5IndexRange, mem_space_idx++);
+			g_debug("mem_range arr %ld %ld", mem_space_range->start, mem_space_range->stop);
 		}
-		else
+		if (file_space_range == NULL)
 		{
-			g_critical("%s NOT implemented !!", G_STRLOC);
-			abort();
+			file_space_range = &g_array_index(file_space_arr, JHDF5IndexRange, file_space_idx++);
+			g_debug("file_range arr %ld %ld", file_space_range->start, file_space_range->stop);
 		}
+		current_count1 = mem_space_range->stop - mem_space_range->start;
+		current_count2 = file_space_range->stop - file_space_range->start;
+		current_count1 = current_count1 < current_count2 ? current_count1 : current_count2;
+		g_debug("mem_range start=%ld count=%ld", mem_space_range->start, current_count1 + 1);
+		g_debug("file_range start=%ld count=%ld", file_space_range->start, current_count1 + 1);
+		(void)local_buf;
+		(void)bytes_written;
+		//TODO			j_distributed_object_write(object->dataset.object, local_buf, data_size, 0, &bytes_written, batch);
+		if (mem_space_range->start + current_count1 == mem_space_range->stop)
+			mem_space_range = NULL;
+		if (file_space_range->start + current_count1 == file_space_range->stop)
+			file_space_range = NULL;
 	}
+	G_DEBUG_HERE();
+	abort();
 	if (!j_batch_execute(batch))
 	{
 		goto _error;
