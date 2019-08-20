c484fe8aefc9b2202f5a9209814e0fdd4476fb6b
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2019-08-20 15:46:26 +0200

    a

--- enzo-all/slurm__FLD_FLD_LWRadParametersenzo.sh
+++ enzo-all/slurm__FLD_FLD_LWRadParametersenzo.sh
@@ -5,38 +5,32 @@
 #SBATCH --nodes=1
 #SBATCH --time=01:30:00
 
-thepath=${PWD}
-name=slurm__FLD_FLD_LWRadParametersenzo
-builddir="hdf-julea"
 tmpdir=/dev/shm/warnke/julea
 
 rm -rf $tmpdir
 mkdir -p $tmpdir
 
+echo slurm__FLD_FLD_LWRadParametersenzo.sh
 echo $tmpdir
-echo $thepath
-echo $name
-echo $builddir
 
-${thepath}/build-${builddir}/tools/julea-config --user   --object-servers="$(hostname)" --kv-servers="$(hostname)"   --db-servers="$(hostname)"   --object-backend=posix --object-component=server --object-path="${tmpdir}/server-object"   --kv-backend=sqlite --kv-component=server --kv-path="${tmpdir}/server-kv"   --db-backend=sqlite --db-component=server --db-path="memory"
+cat ${HOME}/.config/julea/julea-slurm__FLD_FLD_LWRadParametersenzo
 
-mv ${HOME}/.config/julea/julea ${HOME}/.config/julea/julea-${name}
-
-cat ${HOME}/.config/julea/julea-${name}
-
-export LD_LIBRARY_PATH=${thepath}/prefix-${builddir}/lib/:$LD_LIBRARY_PATH
-export JULEA_CONFIG=${HOME}/.config/julea/julea-${name}
+export LD_LIBRARY_PATH=${HOME}/julea/prefix-hdf-julea/lib/:$LD_LIBRARY_PATH
+export JULEA_CONFIG=${HOME}/.config/julea/julea-slurm__FLD_FLD_LWRadParametersenzo
 export HDF5_VOL_JULEA=1
 export HDF5_PLUGIN_PATH=${HOME}/julea/prefix-hdf-julea/lib
 export LD_PRELOAD="$(locate libSegFault.so | tail -n 1)"
 export SEGFAULT_SIGNALS="all"
 export J_TIMER_DB="${HOME}/julea/slurm__FLD_FLD_LWRadParametersenzo.sqlite"
 
-sleep 0.5s
+${HOME}/julea/build-hdf-julea/tools/julea-config --user   --object-servers="$(hostname)" --kv-servers="$(hostname)"   --db-servers="$(hostname)"   --object-backend=posix --object-component=server --object-path="${tmpdir}/server-object"   --kv-backend=sqlite --kv-component=server --kv-path="${tmpdir}/server-kv"   --db-backend=sqlite --db-component=server --db-path="memory"
+mv ${HOME}/.config/julea/julea ${HOME}/.config/julea/julea-slurm__FLD_FLD_LWRadParametersenzo
 
-./build-hdf-julea/server/julea-server &
+sleep 5s
 
-sleep 0.5s
+${HOME}/julea/build-hdf-julea/server/julea-server &
+
+sleep 5s
 
 cp -r ${HOME}/enzo-dev/run/./FLD/* $tmpdir
 cd $tmpdir
--- enzo-all/slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo.sh
+++ enzo-all/slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo.sh
@@ -5,38 +5,32 @@
 #SBATCH --nodes=1
 #SBATCH --time=01:30:00
 
-thepath=${PWD}
-name=slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo
-builddir="hdf-julea"
 tmpdir=/dev/shm/warnke/julea
 
 rm -rf $tmpdir
 mkdir -p $tmpdir
 
+echo slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo.sh
 echo $tmpdir
-echo $thepath
-echo $name
-echo $builddir
 
-${thepath}/build-${builddir}/tools/julea-config --user   --object-servers="$(hostname)" --kv-servers="$(hostname)"   --db-servers="$(hostname)"   --object-backend=posix --object-component=server --object-path="${tmpdir}/server-object"   --kv-backend=sqlite --kv-component=server --kv-path="${tmpdir}/server-kv"   --db-backend=sqlite --db-component=server --db-path="memory"
+cat ${HOME}/.config/julea/julea-slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo
 
-mv ${HOME}/.config/julea/julea ${HOME}/.config/julea/julea-${name}
-
-cat ${HOME}/.config/julea/julea-${name}
-
-export LD_LIBRARY_PATH=${thepath}/prefix-${builddir}/lib/:$LD_LIBRARY_PATH
-export JULEA_CONFIG=${HOME}/.config/julea/julea-${name}
+export LD_LIBRARY_PATH=${HOME}/julea/prefix-hdf-julea/lib/:$LD_LIBRARY_PATH
+export JULEA_CONFIG=${HOME}/.config/julea/julea-slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo
 export HDF5_VOL_JULEA=1
 export HDF5_PLUGIN_PATH=${HOME}/julea/prefix-hdf-julea/lib
 export LD_PRELOAD="$(locate libSegFault.so | tail -n 1)"
 export SEGFAULT_SIGNALS="all"
 export J_TIMER_DB="${HOME}/julea/slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo.sqlite"
 
-sleep 0.5s
+${HOME}/julea/build-hdf-julea/tools/julea-config --user   --object-servers="$(hostname)" --kv-servers="$(hostname)"   --db-servers="$(hostname)"   --object-backend=posix --object-component=server --object-path="${tmpdir}/server-object"   --kv-backend=sqlite --kv-component=server --kv-path="${tmpdir}/server-kv"   --db-backend=sqlite --db-component=server --db-path="memory"
+mv ${HOME}/.config/julea/julea ${HOME}/.config/julea/julea-slurm__Hydro_Hydro-1D_Toro-4-ShockTubeAMR_Toro-4-ShockTubeAMRenzo
 
-./build-hdf-julea/server/julea-server &
+sleep 5s
 
-sleep 0.5s
+${HOME}/julea/build-hdf-julea/server/julea-server &
+
+sleep 5s
 
 cp -r ${HOME}/enzo-dev/run/./Hydro/Hydro-1D/Toro-4-ShockTubeAMR/* $tmpdir
 cd $tmpdir
